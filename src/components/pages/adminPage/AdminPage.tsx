744b008cc8d241e04745761caadf69dc6d758c62
SavaOurl <dendiroblek@gmail.com>
2023-10-31 17:56:21 +0400

    added card for adminpanel

--- src/components/pages/adminPage/AdminPage.tsx
+++ src/components/pages/adminPage/AdminPage.tsx
@@ -1,15 +1,37 @@
-import { useAppSelector } from '../../../hooks/redux';
-
+import { useAppSelector, useAppDispatch } from '../../../hooks/redux';
+import {useEffect} from 'react'
+import AdminCard from '../../adminCard/AdminCard';
 
 import './AdminPage.scss';
 import './AdminPage-media.scss';
+import { getProducts } from '../../../store/adminSlice';
+
+
+//нужно будет сделать миддлевейр,который проверяет является ли запрос от админа
 
 const AdminPage: React.FC = () => {
+    //настроить react helmet
 
-    const role = useAppSelector(store => store.loginStates.role);
+    const dispatch = useAppDispatch()
+    const { products } = useAppSelector(store => store.adminState);
+
+    useEffect(() => {
+      dispatch(getProducts())
+    }, [])
     
+    
+
     return(
-        <h1>This is AdminPage</h1>
+        <div className="container">
+            <h1 className="adminPage__title">Админ панель</h1>
+            <div className="adminPage__items">
+                {products.map((product) => {
+                   return(
+                    <AdminCard key={product.productID} dataProduct={product}/>
+                   ) 
+                })}
+            </div>
+        </div>
     )
 }
 export default AdminPage
